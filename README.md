5dd66f43d58a2a6b83d3b018a89bbc9941e3ee6b
Offir Golan <offirgolan@yahoo-inc.com>
2016-03-18 14:41:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# ember-bootstrap-floating-labels
+
+## Running
+
+* `ember server`
+* Visit your app at http://localhost:4200.
+
+## Running Tests
+
+* `ember test`
+* `ember test --server`
+
+## Building
+
+* `ember build`
+
+For more information on using ember-cli, visit [http://www.ember-cli.com/](http://www.ember-cli.com/).
+
+[npm]: https://www.npmjs.org/package/ember-bootstrap-floating-labels
+[npm-badge]: https://img.shields.io/npm/v/ember-bootstrap-floating-labels.svg?style=flat-square
+[travis]: https://travis-ci.org/offirgolan/ember-bootstrap-floating-labels
+[travis-badge]: https://img.shields.io/travis/offirgolan/ember-bootstrap-floating-labels/master.svg?style=flat-square
+[embadge]: http://embadge.io/
+[ember-badge]: http://embadge.io/v1/badge.svg?start=1.13.0
---
+++ addon/initializers/form-element.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import FormElement from 'ember-bootstrap/components/bs-form-element';
+
+const {
+  isEmpty,
+  computed
+} = Ember;
+
+export default function() {
+  FormElement.reopen({
+    classNameBindings: ['floatLabel:floating-label'],
+
+    floatLabel: computed('form.formLayout', function() {
+      return this.get('form.formLayout') === 'vertical';
+    }),
+
+    didInsertElement() {
+      this._super(...arguments);
+      if(this.get('floatLabel')) {
+        this.$('.form-control').on('focus.bs-floating blur.bs-floating', (e) => {
+          this.$().toggleClass('focused', (e.type === 'focus' || !isEmpty(this.get('value'))));
+        });
+      }
+    },
+
+    willDestroyElement() {
+      this.$().off('focus.bs-floating blur.bs-floating');
+    }
+  });
+}
